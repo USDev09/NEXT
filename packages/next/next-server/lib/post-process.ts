vercel/next.js

commit 57e156bc49024fda19ffdffb1ed4befc4a07c2c3

Making font optimization as default (#19758)
- Making font optimizations as default
- Re-enabling tests
- Fixes #19159

packages/next/next-server/lib/post-process.ts
@@ -6,7 +6,6 @@ const MAXIMUM_IMAGE_PRELOADS = 2
 const IMAGE_PRELOAD_SIZE_THRESHOLD = 2500
 
 type postProcessOptions = {
-  optimizeFonts: boolean
   optimizeImages: boolean
 }
 
@@ -143,10 +142,20 @@ class FontOptimizerMiddleware implements PostProcessMiddleware {
         continue
       }
       const fontContent = options.getFontDefinition(url)
-      result = result.replace(
-        '</head>',
-        `<style data-href="${url}">${fontContent}</style></head>`
-      )
+      if (!fontContent) {
+        /**
+         * In case of unreachable font definitions, fallback to default link tag.
+         */
+        result = result.replace(
+          '</head>',
+          `<link rel="stylesheet" href="${url}"/></head>`
+        )
+      } else {
+        result = result.replace(
+          '</head>',
+          `<style data-href="${url}">${fontContent}</style></head>`
+        )
+      }
     }
     return result
   }
@@ -251,13 +260,7 @@ function sourceIsSupportedType(imgSrc: string): boolean {
 }
 
 // Initialization
-registerPostProcessor(
-  'Inline-Fonts',
-  new FontOptimizerMiddleware(),
-  // Using process.env because passing Experimental flag through loader is not possible.
-  // @ts-ignore
-  (options) => options.optimizeFonts || process.env.__NEXT_OPTIMIZE_FONTS
-)
+registerPostProcessor('Inline-Fonts', new FontOptimizerMiddleware(), () => true)
 
 registerPostProcessor(
   'Preload Images',

packages/next/build/webpack-config.ts
@@ -970,9 +970,6 @@ export default async function getBaseWebpackConfig(
         'process.env.__NEXT_REACT_MODE': JSON.stringify(
           config.experimental.reactMode
         ),
-        'process.env.__NEXT_OPTIMIZE_FONTS': JSON.stringify(
-          config.experimental.optimizeFonts && !dev
-        ),
         'process.env.__NEXT_OPTIMIZE_IMAGES': JSON.stringify(
           config.experimental.optimizeImages
         ),
@@ -1077,8 +1074,7 @@ export default async function getBaseWebpackConfig(
         new ProfilingPlugin({
           tracer,
         }),
-      config.experimental.optimizeFonts &&
-        !dev &&
+      !dev &&
         isServer &&
         (function () {
           const {
@@ -1166,7 +1162,6 @@ export default async function getBaseWebpackConfig(
       plugins: config.experimental.plugins,
       reactStrictMode: config.reactStrictMode,
       reactMode: config.experimental.reactMode,
-      optimizeFonts: config.experimental.optimizeFonts,
       optimizeImages: config.experimental.optimizeImages,
       scrollRestoration: config.experimental.scrollRestoration,
       basePath: config.basePath,

packages/next/build/webpack/loaders/next-serverless-loader/page-handler.ts
@@ -370,15 +370,12 @@ export function getPageHandler(ctx: ServerlessHandlerCtx) {
 
       const previewData = tryGetPreviewData(req, res, options.previewProps)
       const isPreviewMode = previewData !== false
-      if (process.env.__NEXT_OPTIMIZE_FONTS) {
-        renderOpts.optimizeFonts = true
-        /**
-         * __webpack_require__.__NEXT_FONT_MANIFEST__ is added by
-         * font-stylesheet-gathering-plugin
-         */
-        // @ts-ignore
-        renderOpts.fontManifest = __webpack_require__.__NEXT_FONT_MANIFEST__
-      }
+      /**
+       * __webpack_require__.__NEXT_FONT_MANIFEST__ is added by
+       * font-stylesheet-gathering-plugin
+       */
+      // @ts-ignore
+      renderOpts.fontManifest = __webpack_require__.__NEXT_FONT_MANIFEST__
       let result = await renderToHTML(
         req,
         res,

packages/next/export/index.ts
@@ -484,7 +484,6 @@ Read more: https://err.sh/next.js/export-image-api`
         subFolders,
         buildExport: options.buildExport,
         serverless: isTargetLikeServerless(nextConfig.target),
-        optimizeFonts: nextConfig.experimental.optimizeFonts,
         optimizeImages: nextConfig.experimental.optimizeImages,
         optimizeCss: nextConfig.experimental.optimizeCss,
       })

packages/next/export/worker.ts
@@ -47,7 +47,6 @@ interface ExportPageInput {
   serverRuntimeConfig: string
   subFolders: string
   serverless: boolean
-  optimizeFonts: boolean
   optimizeImages: boolean
   optimizeCss: any
 }
@@ -67,7 +66,6 @@ interface RenderOpts {
   ampSkipValidation?: boolean
   hybridAmp?: boolean
   inAmpMode?: boolean
-  optimizeFonts?: boolean
   optimizeImages?: boolean
   optimizeCss?: any
   fontManifest?: FontManifest
@@ -92,7 +90,6 @@ export default async function exportPage({
   serverRuntimeConfig,
   subFolders,
   serverless,
-  optimizeFonts,
   optimizeImages,
   optimizeCss,
 }: ExportPageInput): Promise<ExportPageResults> {
@@ -250,14 +247,10 @@ export default async function exportPage({
           {
             ampPath: renderAmpPath,
             /// @ts-ignore
-            optimizeFonts,
-            /// @ts-ignore
             optimizeImages,
             /// @ts-ignore
             optimizeCss,
-            fontManifest: optimizeFonts
-              ? requireFontManifest(distDir, serverless)
-              : null,
+            fontManifest: requireFontManifest(distDir, serverless),
             locale: locale!,
             locales: renderOpts.locales!,
           },
@@ -295,15 +288,6 @@ export default async function exportPage({
         html = components.Component
         queryWithAutoExportWarn()
       } else {
-        /**
-         * This sets environment variable to be used at the time of static export by head.tsx.
-         * Using this from process.env allows targeting both serverless and SSR by calling
-         * `process.env.__NEXT_OPTIMIZE_FONTS`.
-         * TODO(prateekbh@): Remove this when experimental.optimizeFonts are being cleaned up.
-         */
-        if (optimizeFonts) {
-          process.env.__NEXT_OPTIMIZE_FONTS = JSON.stringify(true)
-        }
         if (optimizeImages) {
           process.env.__NEXT_OPTIMIZE_IMAGES = JSON.stringify(true)
         }
@@ -315,12 +299,9 @@ export default async function exportPage({
           ...renderOpts,
           ampPath: renderAmpPath,
           params,
-          optimizeFonts,
           optimizeImages,
           optimizeCss,
-          fontManifest: optimizeFonts
-            ? requireFontManifest(distDir, serverless)
-            : null,
+          fontManifest: requireFontManifest(distDir, serverless),
           locale: locale as string,
         }
         // @ts-ignore

packages/next/next-server/lib/head.tsx
@@ -136,7 +136,7 @@ function reduceComponents(
     .reverse()
     .map((c: React.ReactElement<any>, i: number) => {
       const key = c.key || i
-      if (process.env.__NEXT_OPTIMIZE_FONTS && !props.inAmpMode) {
+      if (process.env.NODE_ENV !== 'development' && !props.inAmpMode) {
         if (
           c.type === 'link' &&
           c.props['href'] &&

packages/next/next-server/server/config.ts
@@ -53,7 +53,6 @@ const defaultConfig: { [key: string]: any } = {
     workerThreads: false,
     pageEnv: false,
     productionBrowserSourceMaps: false,
-    optimizeFonts: false,
     optimizeImages: false,
     optimizeCss: false,
     scrollRestoration: false,

packages/next/next-server/server/next-server.ts
@@ -147,7 +147,6 @@ export default class Server {
     customServer?: boolean
     ampOptimizerConfig?: { [key: string]: any }
     basePath: string
-    optimizeFonts: boolean
     images: string
     fontManifest: FontManifest
     optimizeImages: boolean
@@ -202,11 +201,9 @@ export default class Server {
       ampOptimizerConfig: this.nextConfig.experimental.amp?.optimizer,
       basePath: this.nextConfig.basePath,
       images: JSON.stringify(this.nextConfig.images),
-      optimizeFonts: this.nextConfig.experimental.optimizeFonts && !dev,
-      fontManifest:
-        this.nextConfig.experimental.optimizeFonts && !dev
-          ? requireFontManifest(this.distDir, this._isLikeServerless)
-          : null,
+      fontManifest: !dev
+        ? requireFontManifest(this.distDir, this._isLikeServerless)
+        : null,
       optimizeImages: this.nextConfig.experimental.optimizeImages,
       optimizeCss: this.nextConfig.experimental.optimizeCss,
     }
@@ -268,12 +265,9 @@ export default class Server {
     /**
      * This sets environment variable to be used at the time of SSR by head.tsx.
      * Using this from process.env allows targetting both serverless and SSR by calling
-     * `process.env.__NEXT_OPTIMIZE_FONTS`.
-     * TODO(prateekbh@): Remove this when experimental.optimizeFonts are being clened up.
+     * `process.env.__NEXT_OPTIMIZE_IMAGES`.
+     * TODO(atcastle@): Remove this when experimental.optimizeImages are being clened up.
      */
-    if (this.renderOpts.optimizeFonts) {
-      process.env.__NEXT_OPTIMIZE_FONTS = JSON.stringify(true)
-    }
     if (this.renderOpts.optimizeImages) {
       process.env.__NEXT_OPTIMIZE_IMAGES = JSON.stringify(true)
     }

USDev09/NEXT

commit 0c2da1837dc9267f481f96b70d756b895f96dd4c

Fix failing Azure tests (#19877)
Noticed Azure tests have started failing after https://github.com/vercel/next.js/pull/19758 due to the middleware taking longer than the allotted threshold which adds unexpected logs to stderr. This attempts to resolve these test failures by increasing the allotted time for the middleware when running on Azure. 

x-ref: https://dev.azure.com/nextjs/next.js/_build/results?buildId=22617&view=logs&jobId=5ee44de4-e310-59d9-0ba5-4a271a649fb0&j=5ee44de4-e310-59d9-0ba5-4a271a649fb0&t=af3fb693-3771-5a1a-ec97-2ba4cc77f25c
x-ref: https://dev.azure.com/nextjs/next.js/_build/results?buildId=22619&view=logs&jobId=5ee44de4-e310-59d9-0ba5-4a271a649fb0&j=5ee44de4-e310-59d9-0ba5-4a271a649fb0&t=af3fb693-3771-5a1a-ec97-2ba4cc77f25c

packages/next/next-server/lib/post-process.ts
@@ -1,7 +1,8 @@
 import { parse, HTMLElement } from 'node-html-parser'
 import { OPTIMIZED_FONT_PROVIDERS } from './constants'
 
-const MIDDLEWARE_TIME_BUDGET = 10
+const MIDDLEWARE_TIME_BUDGET =
+  parseInt(process.env.__POST_PROCESS_MIDDLEWARE_TIME_BUDGET || '', 10) || 10
 const MAXIMUM_IMAGE_PRELOADS = 2
 const IMAGE_PRELOAD_SIZE_THRESHOLD = 2500
 

run-tests.js
@@ -195,6 +195,7 @@ const configuredTestTypes = [UNIT_TEST_EXT]
             ...(isAzure
               ? {
                   HEADLESS: 'true',
+                  __POST_PROCESS_MIDDLEWARE_TIME_BUDGET: '50',
                 }
               : {}),
             ...(usePolling